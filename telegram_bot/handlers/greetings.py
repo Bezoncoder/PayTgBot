a96cae809610448dfa4729d0828107aeb01dcd23
bezon <you@example.com>
2026-04-01 18:48:25 +0300

    fix

--- telegram_bot/handlers/greetings.py
+++ telegram_bot/handlers/greetings.py
@@ -26,6 +26,7 @@ router = Router()
 START_CAPTION = (f'🚀 **Добро пожаловать!**\n\n'
                 f'✅ Отличный выбор!\n\n'
                 f'📡 Самый быстрый VPN с серверами по всему миру и защитой.\n\n'
+                f'🗝️ Один ключ на два устройства.\n\n'
                 f'🌐 Наш Сайт: <a href="https://quantumturbovpn.ddns.net/">QuantumTurboVPN</a>.\n\n'
                 f'📺 Как подключить: <a href="https://t.me/QuantumTurboVPN/351">Смотреть видео</a>.\n\n'
                 f'🛠️️ Техподдержка 24/7 в чате.\n\n'
