6110595d9963b97c434346208f8e3cf0dbf6d956
bezon <you@example.com>
2026-09-12 17:26:06 +0400

    fix

--- telegram_bot/handlers/get_account_summary.py
+++ telegram_bot/handlers/get_account_summary.py
@@ -78,7 +78,14 @@ async def check_account_summary_from_message(message: Message, state: FSMContext
     #TODO ДЕЛАЕМ ЗАПРОС В БД, ЧТОБЫ УЗНАТЬ ПОДПИСКИ!!!!!!!!!!!!!!!!!!!
 
     # GET USER ENROLMENTS
-
+    if message.forward_from is None:
+        await message.bot.delete_message(chat_id=message.from_user.id, message_id=message.message_id)
+        await message.bot.edit_message_caption(chat_id=message.from_user.id,
+                                               message_id=message.message_id,
+                                               caption=(f"⚠️ У пользователя закрыт Telegram ID.\n\n"
+                                                        f"Не удалось определить автора пересланного сообщения.\n"
+                                                        f"Попросите пользователя прислать свой Telegram ID."))
+        return
     user_info = await get_user_info_by_tg_id(tg_user_id=int(message.forward_from.id))
 
     # user_enrolments = [EnrollmentPydantic]
